4505d7d289f092fe85537dba929181eb19645c54
Anton <voevodin_anton_2000@mail.ru>
2021-06-03 14:19:01 +0400

    Осторожно неравбочий код

--- src/main/java/com/netcracker/web/violations/config/DataSourceConfiguration.java
+++ src/main/java/com/netcracker/web/violations/config/DataSourceConfiguration.java
@@ -3,6 +3,6 @@ package com.netcracker.web.violations.config;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnCla
+
 public class DataSourceConfiguration {
 }
--- src/main/java/com/netcracker/web/violations/dao/CarDAOImpl.java
+++ src/main/java/com/netcracker/web/violations/dao/CarDAOImpl.java
@@ -3,7 +3,6 @@ package com.netcracker.web.violations.dao;
 import com.netcracker.web.violations.model.Car;
 import com.netcracker.web.violations.model.Violation;
 import com.netcracker.web.violations.model.ViolationOutput;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -20,9 +19,6 @@ public class CarDAOImpl implements CarDAO {
     private static String username = "postgres";
     private static String password = "avoeva";
     private static Connection connection;
-
-    @Autowired
-    DataSource dataSource;
 
     {
         System.out.println("Connecting...");
@@ -45,7 +41,6 @@ public class CarDAOImpl implements CarDAO {
     @Override
     public void save(Car car) {
         try {
-            dataSource.getConnection();
             PreparedStatement preparedStatement =  connection.prepareStatement("INSERT INTO Car (number, model, owner) VALUES(?,?,?)");
 
             preparedStatement.setString(1, car.getNumber());
@@ -58,20 +53,6 @@ public class CarDAOImpl implements CarDAO {
         }
     }
 
-    public boolean checkUnique(Car car){
-        PreparedStatement preparedStatement = null;
-        boolean result = false;
-        try {
-            preparedStatement = connection.prepareStatement("SELECT * FROM Car WHERE number=?");
-            preparedStatement.setString(1, car.getNumber());
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if(!resultSet.next())
-                result = true;
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return result;
-    }
     @Override
     public void update(int id, Car car) {
         try {
